f3162beb57b8b7139f14a2ec6420a9e6ea74069d
Vlad Kopalov <timevk@gmail.com>
2020-02-03 19:19:00 +0300

    New format of output data

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,35 @@
+export type TCalendarResult = {
+  before: {
+    count: number
+    start: number
+  },
+  now: {
+    end: number
+  }
+}
+
 function getWeekDay (day: number) {
   return day === 0 ? 7 : day
 }
 
-export function getCalendar (date: Date) {
-  const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0)
-  const tmpDate = new Date(date.getFullYear(), date.getMonth(), 1)
-  const beforeDates = []
-  let count = getWeekDay(tmpDate.getDay()) - 1
-  if (count > 0) {
-    const lastDayPreMonth = new Date(date.getFullYear(), date.getMonth(), -count)
-    let time = lastDayPreMonth.getTime()
-    for (let dateIndex = 1; dateIndex <= count; dateIndex += 1) {
-      lastDayPreMonth.setTime(time + dateIndex * 24 * 60 * 60 * 1000)
-      beforeDates.push(lastDayPreMonth.getDate())
-    }
-  }
+/**
+ * Возвращет модель месяца
+ *
+ * @param date Для какого года и месяца построить модель
+ */
+export function getCalendar (date: Date): TCalendarResult {
+  const lastDayOfCurrentMonth = new Date(date.getFullYear(), date.getMonth() + 1, 0)
+  const firstDayOfCurrentMonth = new Date(date.getFullYear(), date.getMonth(), 1)
+  const lastDayPreMonth = new Date(date.getFullYear(), date.getMonth(), 0)
+  const countOfPrevDaysOfCurrentMonthInWeek = getWeekDay(firstDayOfCurrentMonth.getDay()) - 1
 
   return {
-    before: beforeDates, count: lastDay.getDate()
+    before: {
+      start: countOfPrevDaysOfCurrentMonthInWeek <= 0 ? 0 : (lastDayPreMonth.getDate() - countOfPrevDaysOfCurrentMonthInWeek + 1),
+      count: countOfPrevDaysOfCurrentMonthInWeek,
+    },
+    now: {
+      end: lastDayOfCurrentMonth.getDate()
+    }
   }
 }
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,17 +1,25 @@
 import { getCalendar } from '../src'
 
+test('calendar 01.03.2020', () => {
+  expect(getCalendar(new Date(2020, 2))).toEqual({
+      before: { start: 24, count: 6 },
+      now: { end: 31 },
+    }
+  )
+})
+
 test('calendar 01.02.2020', () => {
   expect(getCalendar(new Date(2020, 1))).toEqual({
-      before: [27, 28, 29, 30, 31],
-      count: 29,
+      before: { start: 27, count: 5 },
+      now: { end: 29 },
     }
   )
 })
 
 test('calendar 01.10.2012', () => {
   expect(getCalendar(new Date(2012, 9))).toEqual({
-      before: [],
-      count: 31,
+      before: { start: 0, count: 0 },
+      now: { end: 31 },
     }
   )
 })
